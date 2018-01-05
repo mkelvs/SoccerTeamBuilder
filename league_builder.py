537902443b5ae5c8ab3b11e14ef7c6fb87de9172
Mark Kelvin Libario <mkelvin.libario@gmail.com>
2018-01-04 23:40:43 -0500

    Creating functions
    csv_to_list function created.
    sort_players function created

--- league_builder.py
+++ league_builder.py
@@ -1,22 +1,35 @@
 import csv
 
-experienced_players = []
-regular_players = []
-dragons_team = []
-sharks_team = []
-raptors_team = []
+#Reads the csv file and convert it to a list
+def csv_to_list():
+    with open("soccer_players.csv", newline='') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        csv_list = list(csv_reader)
+        return csv_list
 
-with open("soccer_players.csv", "r") as csv_file:
-    csv_reader = csv.DictReader(csv_file)
 
-    for player in csv_reader:
+
+#Sort players based on their experience
+def sort_players(players):
+    experienced_players = []
+    regular_players = []
+    for player in players:
         #Gathers all Experienced players together and all in list experienced_players
         if player["Soccer Experience"] == "YES":
             experienced_players.append(player)
         else:
             # Gathers all Experienced players together and all in list regular_players
             regular_players.append(player)
+    return experienced_players, regular_players
 
+
+all_items = csv_to_list()
+experienced_players, regular_players = sort_players(all_items)
+
+print("Experienced Players: {}".format(experienced_players))
+
+
+"""
     for players in experienced_players:
         # Each experienced player in list experienced_players is placed equally on each team
         if players not in dragons_team and len(dragons_team) < 3:
@@ -35,7 +48,4 @@ with open("soccer_players.csv", "r") as csv_file:
         elif players not in raptors_team and len(raptors_team) < 6:
             raptors_team.append(players)
 
-
-
-
-
+"""
