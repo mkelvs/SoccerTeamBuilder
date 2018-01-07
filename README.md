d99e9c1c78c6ac64f1a8f8cc0cfe23c7878e6bce
Mark Kelvin Libario <mkelvin.libario@gmail.com>
2018-01-07 04:41:29 -0500

    Create team roster and letters
    Added a function that creates a text file of the team rosters and a function that writes an individual letter to each players' guardian

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Techdegree Project One
+You have volunteered to be the Coordinator for your town’s youth soccer league. As part of your job you need to divide the 18 children who have signed up for the league into three even teams - Dragons, Sharks and Raptors. In years past, the teams have been unevenly matched, so this year you are doing your best to fix that. For each child, you will have the following information: Name, height (in inches), whether or not they have played soccer before, and their guardians’ names. You'll take a list of these children, divide them into teams and output a text file listing the three teams and the players on them. There are three main tasks you'll need to complete to get this done:
+
+In your Python program, read the data from the supplied CSV file. Store that data in an appropriate data type so that it can be used in the next task.
+
+Create logic that can iterate through all 18 players and assign them to teams such that each team has the same number of players. The number of experienced players on each team should also be the same.
+
+Finally, the program should output a text file named -- teams.txt -- that contains the league roster listing the team name, and each player on the team including the player's information: name, whether they've played soccer before and their guardians' names.
--- league_builder.py
+++ league_builder.py
@@ -1,51 +1,89 @@
-import csv
+import csv, random
 
-#Reads the csv file and convert it to a list
-def csv_to_list():
-    with open("soccer_players.csv", newline='') as csv_file:
+#Creating Lists
+dragons = []
+sharks = []
+raptors = []
+teams = [dragons, sharks, raptors]
+
+
+#Reads the CSV file and returns it as a list.
+def read_csv_file():
+    with open("soccer_players.csv", newline="") as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        csv_list = list(csv_reader)
-        return csv_list
+        players_list = list(csv_reader)
+        return players_list
 
-
-
-#Sort players based on their experience
+# Sort players according to their skills and returns the list of sorted players.
 def sort_players(players):
-    experienced_players = []
-    regular_players = []
+    experienced_players = [] #List of players with soccer experience
+    regular_players = [] # List of players without soccer experience
     for player in players:
-        #Gathers all Experienced players together and all in list experienced_players
         if player["Soccer Experience"] == "YES":
             experienced_players.append(player)
         else:
-            # Gathers all Experienced players together and all in list regular_players
             regular_players.append(player)
     return experienced_players, regular_players
 
+#Distribute players to each teams equally.
+def players_to_teams(experienced, regular, players_list, number_of_teams):
+    team_size = len(players_list)/number_of_teams #calculates the number of players per team
+    if len(dragons) < team_size and len(sharks) < team_size and len(raptors) < team_size:
+        dragons.extend(experienced[:3])
+        dragons.extend(regular[:3])
+        sharks.extend(experienced[3:6])
+        sharks.extend(regular[3:6])
+        raptors.extend(experienced[6:])
+        raptors.extend(regular[6:])
+    for player in dragons:
+        player.update({"Team": "Dragons", "Practice Time": "10:00 AM", "Practice Location": "Gym C"})
+    for player in sharks:
+        player.update({"Team": "Sharks", "Practice Time": "8:00 AM", "Practice Location": "Gym A"})
+    for player in raptors:
+        player.update({"Team": "Raptors", "Practice Time": "9:00 AM", "Practice Location": "Gym B"})
 
-all_items = csv_to_list()
-experienced_players, regular_players = sort_players(all_items)
+    return dragons,sharks,raptors,all_players
 
-print("Experienced Players: {}".format(experienced_players))
+#Creates a text file of the teams roster.
+def create_text():
+    with open("teams.txt", "w") as txt_file:
+        players_list = read_csv_file()
+        experienced, regular = sort_players(players_list)
+        dragons, sharks, raptors, all_players = players_to_teams(experienced, regular, players_list, 3)
+        txt_file.write("          " + "Dragons\n" + "Name | Experience | Guardian(s)\n" + "=" * 20 + "\n")
+        for player in dragons:
+            txt_file.write("{} | {} | {}".format(player["Name"], player["Soccer Experience"], player["Guardian Name(s)"]))
+            txt_file.write("\n")
+        txt_file.write("\n")
+
+        txt_file.write("          " + "Sharks\n" + "Name | Experience | Guardian(s)\n" + "=" * 20 + "\n")
+        for player in sharks:
+            txt_file.write("{} | {} | {}".format(player["Name"], player["Soccer Experience"], player["Guardian Name(s)"]))
+            txt_file.write("\n")
+        txt_file.write("\n")
+
+        txt_file.write("          " + "Raptors\n" + "Name | Experience | Guardian(s)\n" + "=" * 20 + "\n")
+        for player in raptors:
+            txt_file.write("{} | {} | {}".format(player["Name"], player["Soccer Experience"], player["Guardian Name(s)"]))
+            txt_file.write("\n")
+        txt_file.write("\n")
+
+def create_letter():
+    for player in all_players:
+        with open("{}.txt".format(player["Name"].lower().replace(" ", "_")), "w") as txt_file:
+            txt_file.write("Dear {},\n\n".format(player["Guardian Name(s)"]))
+            txt_file.write("We are very excited to welcome {}.\n".format(player["Name"]))
+            txt_file.write("We are looking forward to see you on our first practice day held at {} in {}\n".format(player["Practice Time"], player["Practice Location"]))
+            txt_file.write("See you there!\n\n")
+            txt_file.write("Regards,\n{} Head Coach".format(player["Team"]))
 
 
-"""
-    for players in experienced_players:
-        # Each experienced player in list experienced_players is placed equally on each team
-        if players not in dragons_team and len(dragons_team) < 3:
-            dragons_team.append(players)
-        elif players not in sharks_team and len(sharks_team) <3:
-            sharks_team.append(players)
-        elif players not in raptors_team and len(raptors_team) < 3:
-            raptors_team.append(players)
+#Creating Variables:
+players_list = read_csv_file()
+experienced, regular = sort_players(players_list) #Assigning sorted players to variables
+all_players = experienced + regular
 
-    for players in regular_players:
-        #Each regular players in list regular_players are placed on each team with a max of 6 people
-        if players not in dragons_team and len(dragons_team) < 6:
-            dragons_team.append(players)
-        elif players not in sharks_team and len(sharks_team) <6:
-            sharks_team.append(players)
-        elif players not in raptors_team and len(raptors_team) < 6:
-            raptors_team.append(players)
+players_to_teams(experienced,regular,players_list, 3) #Creating teams
+create_text() #Create a text file for the team rosters
+create_letter() #Create individual letters to players' guardians
 
-"""
